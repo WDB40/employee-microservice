abbe76df60f5d9c2db3f987109de747b95e6d72d
WDB40 <wbrown1640@gmail.com>
2018-09-03 21:52:07 -0500

    Adding EmployeeRepository connection.

--- employee-master/src/main/java/model/EmployeeRepository.java
+++ employee-master/src/main/java/model/EmployeeRepository.java
@@ -1,4 +1,8 @@
 package model;
 
-public interface EmployeeRepository {
+import model.Employee;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
+
 }
