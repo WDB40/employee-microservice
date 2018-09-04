d6659056a267a4c3b56dfcea9089c4b562aad618
WDB40 <wbrown1640@gmail.com>
2018-09-03 21:49:59 -0500

    Adding properties and methods to Employee.

--- employee-master/src/main/java/model/Employee.java
+++ employee-master/src/main/java/model/Employee.java
@@ -1,4 +1,45 @@
 package model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer employeeID;
+    private String firstName;
+    private String lastName;
+    private String jobStatusCode;
+
+    public Integer getEmployeeID() {
+        return employeeID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getJobStatusCode() {
+        return jobStatusCode;
+    }
+
+    public void setJobStatusCode(String jobStatusCode) {
+        this.jobStatusCode = jobStatusCode;
+    }
 }
