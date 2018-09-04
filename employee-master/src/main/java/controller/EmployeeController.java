082ce21607970fefa65a0ca15f25c3689ef53b82
WDB40 <wbrown1640@gmail.com>
2018-09-03 22:22:20 -0500

    Adding EmployeeController class and /hire mapping.

--- employee-master/src/main/java/controller/EmployeeController.java
+++ employee-master/src/main/java/controller/EmployeeController.java
@@ -1,4 +1,30 @@
 package controller;
 
+import model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/master")
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping(path = "/hire")
+    public String hireEmployee(@RequestParam String firstName, @RequestParam String lastName) {
+
+        Employee employee = new Employee();
+        employee.setFirstName(firstName);
+        employee.setLastName(lastName);
+        employee.setJobStatusCode("A");
+
+        employeeRepository.save(employee);
+
+        return "Employee Hired!";
+    }
+
 }
