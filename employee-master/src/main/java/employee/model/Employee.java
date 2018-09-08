f021a35e277e39d3d40ad2f85b7f2af7c1c6fe1d
WDB40 <wbrown1640@gmail.com>
2018-09-08 18:34:57 -0500

    Made column values for Employee not nullable.

--- employee-master/src/main/java/employee/model/Employee.java
+++ employee-master/src/main/java/employee/model/Employee.java
@@ -1,11 +1,6 @@
 package employee.model;
 
-import org.hibernate.annotations.ColumnDefault;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -18,14 +13,17 @@ public class Employee {
 
     @NotNull
     @Size(max=20)
+    @Column(nullable = false)
     private String firstName;
 
     @NotNull
     @Size(max=20)
+    @Column(nullable = false)
     private String lastName;
 
     @NotNull
-    @Size(min=1, max=1)
+    @Size(max=1)
+    @Column(nullable = false)
     private String jobStatusCode;
 
     public Integer getEmployeeID() {
