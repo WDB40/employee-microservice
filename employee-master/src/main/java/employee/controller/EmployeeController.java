81e3c39d5ee138639b34ac3183f23d5439208659
WDB40 <wbrown1640@gmail.com>
2018-09-07 22:48:08 -0500

    Adding PostMapping for hire.

--- employee-master/src/main/java/employee/controller/EmployeeController.java
+++ employee-master/src/main/java/employee/controller/EmployeeController.java
@@ -4,17 +4,31 @@ import employee.model.Employee;
 import employee.model.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class EmployeeController {
-    
+
     @Autowired
     EmployeeRepository employeeRepository;
-    
+
     @GetMapping("/hire")
     public String hireEmployee(Employee employee){
         return "hire";
     }
-    
+
+    @PostMapping("/hire")
+    public String attemptToHire(@Valid Employee employee, BindingResult result){
+
+        if(result.hasErrors()) {
+            return "hire";
+        }
+
+        employeeRepository.save(employee);
+    }
+
 }
