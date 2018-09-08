79167146825fd836f50d4f03760eda7613d0c2b0
WDB40 <wbrown1640@gmail.com>
2018-09-07 22:03:44 -0500

    Refactoring EmployeeController to EmployeeRestController

--- employee-master/src/main/java/employee/controller/EmployeeRestController.java
+++ employee-master/src/main/java/employee/controller/EmployeeRestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/master")
-public class EmployeeController {
+public class EmployeeRestController {
 
     @Autowired
     private EmployeeRepository employeeRepository;
