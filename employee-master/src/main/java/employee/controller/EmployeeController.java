5103ba5bc7d0d990a4371fe4b7a5eaffcdbf38e3
WDB40 <wbrown1640@gmail.com>
2018-09-07 22:28:00 -0500

    Updating the hire view.

--- employee-master/src/main/java/employee/controller/EmployeeController.java
+++ employee-master/src/main/java/employee/controller/EmployeeController.java
@@ -1,7 +1,20 @@
 package employee.controller;
 
+import employee.model.Employee;
+import employee.model.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class EmployeeController {
+    
+    @Autowired
+    EmployeeRepository employeeRepository;
+    
+    @GetMapping("/hire")
+    public String hireEmployee(Employee employee){
+        return "hire";
+    }
+    
 }
